03b2e3ff368bb0bb55b1051ac4b38d4e0fd43b41
Qex90 <Qex90@ukr.net>
2022-05-15 22:00:43 +0300

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+    email: "",
+    message: ""
+}
+
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+    input: document.querySelector(".feedback-form input")
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(e => {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(formData)
+}, 500))
+
+populateForm();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+ }
+
+function populateForm() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    const data = JSON.parse(savedMessage)
+    refs.textarea.value = data.message;
+    refs.input.value = data.email;
+}
